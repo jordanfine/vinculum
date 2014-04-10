ba67fc7c159368c3dcb36163a3f6ea1b801b8443
Sarah Thomas <sarahthomas@Sarah-Thomass-MacBook-Pro.local>
2014-04-10 12:53:56 -0400

    Added bar scene/add main street theme

---
+++ vinculum/Assets/Standard Assets/GlassListener.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+private var gameData : GameObject;
+
+//This is the physical data
+private var gd : GameData;
+
+public var glasses : AudioClip;
+
+function Start () {
+
+	gameData = GameObject.Find("GameData");
+	GameObject.DontDestroyOnLoad(gameData);
+	//This retrieves the GameData object from our GameObject
+	gd = gameData.GetComponent(UserInterface).data;
+
+}
+
+function Update () {
+
+}
+
+function OnMouseDown () {
+
+	AudioSource.PlayClipAtPoint(glasses, transform.position);
+
+}
